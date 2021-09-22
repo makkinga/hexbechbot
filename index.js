b3749e0a4f6cec08c41a18dbf25bd1859000dcb6
Gydo Makkinga <gydo@makkinga.online>
2021-09-22 13:56:21 +0200

    Enabled DM Commands

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 const { Client, Intents } = require('discord.js');
 const { HarmonyAddress } = require('@harmony-js/crypto');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({
+  intents: [
+      Intents.FLAGS.GUILDS,
+      Intents.FLAGS.DIRECT_MESSAGES
+  ]
+});
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
--- register_commands.js
+++ register_commands.js
@@ -19,14 +19,13 @@ const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
   try {
     console.log('Started refreshing application (/) commands.');
 
-
     await Promise.all(
       process.env.GUILD_IDS
         .split(/[ ,]+/)
         .filter(Boolean)
         .map((guild_id) => {
           rest.put(
-            Routes.applicationGuildCommands(process.env.CLIENT_ID, guild_id),
+            Routes.applicationCommands(process.env.CLIENT_ID, guild_id),
             { body: commands },
           );
         })
