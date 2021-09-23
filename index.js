6df91fe44b80b47176ea1db88da54f63abbe4679
Gydo Makkinga <gydo@makkinga.online>
2021-09-23 20:47:16 +0200

    Made some improvements
    As requested by LUUK

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
-require('dotenv').config();
-const { Client, Intents } = require('discord.js');
-const { HarmonyAddress } = require('@harmony-js/crypto');
-const client = new Client({
-  intents: [
-      Intents.FLAGS.GUILDS,
-      Intents.FLAGS.DIRECT_MESSAGES
-  ]
-});
+require('dotenv').config()
+const {Client, Intents, MessageEmbed} = require('discord.js')
+const {HarmonyAddress}                = require('@harmony-js/crypto')
+const client                          = new Client({
+    intents : [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.DIRECT_MESSAGES,
+    ],
+    partials: ['CHANNEL']
+})
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+    console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('messageCreate', async message => {
+    if (message.guildId === null && !message.author.bot) {
+        const embed = new MessageEmbed()
+            .setTitle('Hello, Hero!')
+            .setDescription('Write `/convaddr` and include either your ETH style address or ONE address to be converted to the other')
+
+        await message.channel.send({embeds: [embed]})
+    }
+})
 
 client.on('interactionCreate', async (interaction) => {
-  if (!interaction.isCommand()) return;
+    if (!interaction.isCommand()) return
 
-  if (interaction.commandName === 'convaddr') {
-    try {
-      const address = interaction.options.getString('address', true);
-      const hmyAddress = new HarmonyAddress(address);
+    if (interaction.commandName === 'convaddr') {
+        try {
+            const address    = interaction.options.getString('address', true)
+            const hmyAddress = new HarmonyAddress(address)
 
-      await interaction.reply(
-        '```\n' + 
-        `  0x: ${hmyAddress.basicHex}\n` +
-        ` one: ${hmyAddress.bech32}\n` +
-        '```'
-      );
-    } catch(err) {
-      await interaction.reply(String(err));
+            const embed = new MessageEmbed()
+                .addField(`ETH`, '```' + hmyAddress.basicHex + '```')
+                .addField(`Harmony`, '```' + hmyAddress.bech32 + '```')
+
+            await interaction.channel.send({embeds: [embed]})
+        } catch (error) {
+            await interaction.reply(String(error))
+        }
     }
-  }
-});
+})
 
-client.login(process.env.TOKEN);
+client.login(process.env.TOKEN)
