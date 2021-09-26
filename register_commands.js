96676eeac35f0f57dfcf1a0147c94bc026dd1b2d
Gydo Makkinga <3294611+makkinga@users.noreply.github.com>
2021-09-26 10:51:03 +0200

    Made some improvements (#2)
    * Enabled DM Commands
    
    * Made some improvements
    
    As requested by LUUK
    
    * Removed unwanted files
    
    * Bugfix
    
    Fix for "This interaction failed"

--- register_commands.js
+++ register_commands.js
@@ -1,38 +1,39 @@
-require('dotenv').config();
-const { REST } = require('@discordjs/rest');
-const { ApplicationCommandOptionType, Routes } = require('discord-api-types/v9');
+require('dotenv').config()
+const {REST}                                 = require('@discordjs/rest')
+const {ApplicationCommandOptionType, Routes} = require('discord-api-types/v9')
 
 const commands = [{
-  name: 'convaddr',
-  description: 'Convert Harmony Address!',
-  options: [{
-    name: 'address',
-    type: ApplicationCommandOptionType.String,
-    required: true,
-    description: 'should be clear enough, no?'
-  }]
-}]; 
+    name       : 'convaddr',
+    description: 'Convert Harmony Address!',
+    options    : {
+        name       : 'address',
+        type       : ApplicationCommandOptionType.String,
+        required   : true,
+        description: 'should be clear enough, no?'
+    }
+}]
 
-const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+const rest = new REST({version: '9'}).setToken(process.env.TOKEN);
 
 (async () => {
-  try {
-    console.log('Started refreshing application (/) commands.');
+    try {
+        console.log('Started refreshing application (/) commands.')
 
-    await Promise.all(
-      process.env.GUILD_IDS
-        .split(/[ ,]+/)
-        .filter(Boolean)
-        .map((guild_id) => {
-          rest.put(
-            Routes.applicationCommands(process.env.CLIENT_ID, guild_id),
-            { body: commands },
-          );
-        })
-    );
+        await Promise.all(
+            process.env.GUILD_IDS
+                .split(/[ ,]+/)
+                .filter(Boolean)
+                .map((guild_id) => {
+                    rest.put(
+                        Routes.applicationCommands(process.env.CLIENT_ID),
+                        {body: commands},
+                    )
+                })
+        )
 
-    console.log('Successfully reloaded application (/) commands.');
-  } catch (error) {
-    console.error(error);
-  }
-})();
+        console.log('Successfully reloaded application (/) commands.')
+    } catch (error) {
+        console.error(error)
+    }
+})()
+
